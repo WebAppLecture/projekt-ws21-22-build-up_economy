1cc6b9ab5f93bc6a25f26b492abf8c89756a33f3
Max Graml <maximilian.graml@physik.uni-regensburg.de>
2021-10-08 16:29:30 +0200

    cleaning code

--- src/comp/transition.js
+++ src/comp/transition.js
@@ -1,5 +1,7 @@
 import { Database } from "./database.js";
 
+
+//These lines take care of the switching between the windows by arrows at the bottom or button clicks on the top
 function show(elem,list) {
     list.forEach(e => e.classList.add("hidden"));
     document.querySelector("#"+elem).classList.remove("hidden")
@@ -12,23 +14,19 @@ function getUnhidden(tbls) {
     })
     return res;
 }
-function clickPopup() {
-    console.log("opening manual in popup");
-    window.open('./src/manual.html','manual','height=700,width=500');
-};
 
 let btns_top = Array.from(document.querySelectorAll(".controls > button")),
-    tbls = Array.from(document.querySelectorAll(".menu > .grid")),
-    btn_weekpsd = document.querySelector("#primary"),
-    btn_man = document.querySelector("#secondary"),
-    btn_left = document.querySelector(".previous"),
-    btn_right = document.querySelector(".next"),
-    btn_settings = document.querySelector("#reset"),
+    tbls = Array.from(document.querySelectorAll(".menu > .grid"));
+    
+document.querySelector(".previous").addEventListener("click", () => show(btns_top[(getUnhidden(tbls)+2)%3].dataset.table,tbls))
+document.querySelector(".next").addEventListener("click", () => show(btns_top[(getUnhidden(tbls)+1)%3].dataset.table,tbls))
+btns_top.forEach(btn => btn.addEventListener("click", ()=> show(btn.dataset.table,tbls)));
+
+
+//This section allows us to call the settings and back ONLY IF a json file is loaded
+let btn_settings = document.querySelector("#reset"),
     menus = Array.from(document.querySelectorAll(".menu"));
 
-btn_left.addEventListener("click", () => show(btns_top[(getUnhidden(tbls)+2)%3].dataset.table,tbls))
-btn_right.addEventListener("click", () => show(btns_top[(getUnhidden(tbls)+1)%3].dataset.table,tbls))
-btns_top.forEach(btn => btn.addEventListener("click", ()=> show(btn.dataset.table,tbls)));
 btn_settings.addEventListener("click", (btn) => {
     let ldfst = document.getElementById("loadfirst");
     if (ldfst.className === "hidden"){
@@ -42,8 +40,16 @@ btn_settings.addEventListener("click", (btn) => {
     };
     
 });
-btn_weekpsd.addEventListener("click", () => DataAs.weekPassed());
-btn_man.addEventListener("click", () => document.createElement("a").href = clickPopup());
+
+
+//Initializes the moon and sun functionality
+document.querySelector("#primary").addEventListener("click", () => DataAs.weekPassed());
+document.querySelector("#secondary").addEventListener("click", () => {
+        document.createElement("a").href = window.open('./src/manual.html','manual','height=700,width=500')
+    });
+
+
+//Loads the main file
 let DataAs = new Database();
 
 
